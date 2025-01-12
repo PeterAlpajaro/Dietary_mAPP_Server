dde3fe74507bba4b4bb9643826db3d0b5c1da23b
Peter Alpajaro <peter.alpajarojr@gmail.com>
2025-01-12 03:38:38 -0500

    Streamlined data structure

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express();
 // const port = process.env.PORT || 8080;
 app.use(express.json());
 
-var restaurant_number = 20;
-var item_number = 20;
+var restaurant_number = 40;
+var item_number = 40;
 
 console.log(process.env.MYSQL_URL);
 console.log("hello world");
